4b03219818522e72b8bdca8e0b871727bb0d77e2
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-08-30 16:11:43 +0300

    feat: enhance Supabase client configuration with authentication options and session management

--- client/lib/supabase.ts
+++ client/lib/supabase.ts
@@ -1,5 +1,27 @@
-import { createClient } from '@supabase/supabase-js';
+import { AppState, Platform } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient, processLock } from '@supabase/supabase-js';
+
 const apiUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const apiKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(apiUrl, apiKey);
+export const supabase = createClient(apiUrl, apiKey, {
+  auth: {
+    ...(Platform.OS !== 'web' ? { storage: AsyncStorage } : {}),
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+});
+
+if (Platform.OS !== 'web') {
+  AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+      supabase.auth.startAutoRefresh();
+    } else {
+      supabase.auth.stopAutoRefresh();
+    }
+  });
+}
