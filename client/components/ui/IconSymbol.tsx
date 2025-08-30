7eccdac8a1d879f7a276e0c5f9d73b8be91f17f0
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-08-30 16:56:45 +0300

    feat: add additional icon mappings for 'plus' and 'cross' in IconSymbol component

--- client/components/ui/IconSymbol.tsx
+++ client/components/ui/IconSymbol.tsx
@@ -18,6 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'plus': 'add',
+  'cross': 'close',
 } as IconMapping;
 
 /**
